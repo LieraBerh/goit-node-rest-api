0fec5328c6d3bb1584ae2f03e047c3ba4a255e72
LieraBerh <lierrastern@gmail.com>
2024-08-07 11:11:09 +0200

    start

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import contactsRouter from "./routes/contactsRouter.js";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact page</h1>");
-});
-app.use((req, res, next) => {
-  console.log("Наше проміжне ПЗ");
-  next();
-});
-app.get("/contact/:id", (req, res) => {
-  res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`);
+  console.log("Server is running. Use our API on port: 3000");
 });
