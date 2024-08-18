9d15b7e393b3bd8c8030521ed628978b5c915c67
LieraBerh <lierrastern@gmail.com>
2024-08-18 19:21:19 +0200

    add dotenv config

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
+import "dotenv/config";
 
 import contactsRouter from "./routes/contactRouter.js";
 
